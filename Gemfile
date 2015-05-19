16eff61a525dcb518bd4bf9dd7516a7dac103afb
Seiji Sawayanagi <mostlyfine@gmail.com>
2015-05-20 02:30:32 +0900

    Add install rbenv, ruby recipes

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 
 gem "itamae"
 gem "serverspec"
+gem "itamae-plugin-recipe-rtn_rbenv"
---
+++ spec/vagrant/ruby_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+#
+# install rbenv
+describe command('source /etc/profile.d/rbenv.sh; which rbenv') do
+  let(:disable_sudo) { true }
+  its(:stdout) { should match('/usr/local/rbenv/bin/rbenv') }
+end
+
+# install ruby
+describe command("source /etc/profile.d/rbenv.sh; rbenv versions | grep 2.2.2") do
+  let(:disable_sudo) { true }
+  its(:stdout) { should match(/2\.2\.2/) }
+end
