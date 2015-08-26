9367be5570ae92eb9dc5866c5e2b5a62fbb0fc0e
Seiji Sawayanagi <mostlyfine@gmail.com>
2015-08-26 11:27:00 +0900

    tweak vim,keychain install

--- cookbooks/system/tool.rb
+++ cookbooks/system/tool.rb
@@ -2,7 +2,6 @@
 git
 wget
 curl
-vim
 zsh
 screen
 tmux
@@ -12,4 +11,15 @@ ctags
 unzip
 ).each do |pkg|
   package pkg
+end
+
+case node[:platform]
+when /centos|redhat|fedora/
+  package "vim-enhanced"
+  execute "rpm -ivh http://pkgs.repoforge.org/keychain/keychain-2.7.0-1.el6.rf.noarch.rpm" do
+    not_if "rpm -qa | grep keychain"
+  end
+when /ubuntu|debian/
+  package "vim"
+  package "keychain"
 end
