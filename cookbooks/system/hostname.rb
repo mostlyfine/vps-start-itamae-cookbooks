ccdae5d40b5eb439e2240b28f30a26a26dca127e
Seiji Sawayanagi <mostlyfine@gmail.com>
2015-11-11 16:06:24 +0900

    fix only_if command

--- cookbooks/system/hostname.rb
+++ cookbooks/system/hostname.rb
@@ -1,12 +1,11 @@
-file "/etc/sysconfig/network" do
-  action :edit
-  block do |content|
-    content.gsub!(/^NETWORKING.+$/, "NETWORKING=#{node[:hostname]}")
-    content << "NETWORKING=#{node[:hostname]}" unless content.index('NETWORKING')
+if node[:hostname]
+  execute "hostname #{node[:hostname]}"
+  file "/etc/sysconfig/network" do
+    action :edit
+    block do |content|
+      content.gsub!(/^NETWORKING.+$/, "NETWORKING=#{node[:hostname]}")
+      content << "NETWORKING=#{node[:hostname]}" unless content.index('NETWORKING')
+    end
+    only_if "test -f /etc/sysconfig/network"
   end
-  only_if node[:hostname]
-end
-
-execute "hostname #{node[:hostname]}" do
-  only_if node[:hostname]
 end
