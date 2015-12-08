f6f443ce40cd05777288e22075e178ce84385ed0
Seiji Sawayanagi <mostlyfine@gmail.com>
2015-12-08 22:50:39 +0900

    add lv install recipe.

--- cookbooks/system/tool.rb
+++ cookbooks/system/tool.rb
@@ -21,7 +21,12 @@ when /centos|redhat|fedora/
   execute "rpm -ivh http://pkgs.repoforge.org/keychain/keychain-2.7.0-1.el6.rf.noarch.rpm" do
     not_if "rpm -qa | grep keychain"
   end
+
+  execute "rpm -ivh http://apt.sw.be/redhat/el6/en/x86_64/rpmforge/RPMS/lv-4.51-1.el6.rf.x86_64.rpm" do
+    not_if "rpm -qa | grep lv"
+  end
 when /ubuntu|debian/
   package "vim"
   package "keychain"
 end
+
