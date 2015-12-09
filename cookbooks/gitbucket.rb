5ec89241504fd745d38cff92516c6342a4e0ba75
Seiji Sawayanagi <mostlyfine@gmail.com>
2015-12-10 02:25:59 +0900

    Start gitbucket service.

--- cookbooks/gitbucket.rb
+++ cookbooks/gitbucket.rb
@@ -1,5 +1,13 @@
-version = node[:gitbucket] && node[:gitbucket][:version] ? node[:gitbucket][:version] : "3.3"
+version = node[:gitbucket] && node[:gitbucket][:version] ? node[:gitbucket][:version] : "3.9"
+home = node[:gitbucket] && node[:gitbucket][:home] ? node[:gitbucket][:home] : "/var/lib/tomcat/webapps"
+server = node[:gitbucket] && node[:gitbucket][:server] ? node[:gitbucket][:server] : "tomcat"
 
 package "java-1.8.0-openjdk"
-execute "wget https://github.com/takezoe/gitbucket/releases/download/#{version}/gitbucket.war -O /usr/local/src/gitbucket.war"
-execute "java -jar /usr/local/src/gitbucket.war &"
+package server
+execute "wget https://github.com/takezoe/gitbucket/releases/download/#{version}/gitbucket.war -O #{home}/gitbucket.war" do
+  not_if "test -f #{home}/gitbucket.war"
+end
+
+service server do
+  action [:enable, :start]
+end
