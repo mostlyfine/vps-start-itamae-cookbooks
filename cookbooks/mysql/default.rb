a4eba354d25c162f1c849a8e2903abd749ee415f
Seiji Sawayanagi <mostlyfine@gmail.com>
2016-02-10 00:56:44 +0900

    Improved mysql community

--- cookbooks/mysql/default.rb
+++ cookbooks/mysql/default.rb
@@ -1,8 +1,25 @@
+os_version = node[:platform_version].to_i
+db_version = node[:mysql] && node[:mysql][:version] || '5.6'
+db_version = db_version.to_s.gsub(/\./, '')
+
 case node[:platform]
-when /centos|redhat|fedora/
-  package "mysql55-devel"
-  package "mysql55-server"
-  package "mysql55"
+when /centos|redhat/
+  %w{mariadb-libs mysql}.each do |pkg|
+    package pkg do
+      action [:remove]
+    end
+  end
+
+  execute "install mysql community repository" do
+    command "rpm -Uvh http://dev.mysql.com/get/mysql-community-release-el#{os_version}-5.noarch.rpm"
+    not_if "rpm -qa | grep mysql-community"
+  end
+
+  %w{mysql-community-server mysql-community-devel}.each do |mysql|
+    package mysql do
+      options "--enablerepo=mysql#{db_version}-community"
+    end
+  end
 
   service "mysqld" do
     action [:enable, :start]
