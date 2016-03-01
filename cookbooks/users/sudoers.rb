f27d5957e237e5dc866a3b15d7b6f0ac2e7577d5
Seiji Sawayanagi <mostlyfine@gmail.com>
2016-03-01 11:35:11 +0900

    enable to work when centos

--- cookbooks/users/sudoers.rb
+++ cookbooks/users/sudoers.rb
@@ -1,7 +1,9 @@
-group node[:admin_group]
+case node[:platform]
+when /centos|redhat|fedora/
+  group node[:admin_group]
 
-template '/etc/sudoers.d/admin' do
-  owner 'root'
-  group 'root'
+  template '/etc/sudoers.d/admin' do
+    owner 'root'
+    group 'root'
+  end
 end
-
